9e0fd53741312db60bf00c2049bf5814f5c223c6
Daniel Straus <strausd@me.com>
2017-03-07 21:21:46 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -13,12 +13,21 @@ app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
+    
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
     })
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos: todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+})
 
 app.listen(process.env.PORT, () => {
     console.log('Started on port', process.env.PORT);
